0b8d7246fb551fa93b82cee48e77734f482c9d9b
jpdias <jpdias@live.com.pt>
2016-10-18 20:14:26 +0100

    Remove unbabel and added MS Translator. Added where to weather.

--- index.js
+++ index.js
@@ -2,13 +2,12 @@ var restify = require('restify');
 var builder = require('botbuilder');
 var request = require("request");
 
-var user_details = {};
 
 Date.prototype.addHours = function(h) {    
    this.setTime(this.getTime() + (h*60*60*1000)); 
    return this;   
 }
-
+/*
 function unbabel_submit(ub_text, ub_srclng, ub_destlng, callback2){
 	var options = {
 	  url: 'https://sandbox.unbabel.com/tapi/v2/mt_translation/',
@@ -54,7 +53,20 @@ function unbabel_get_result(uid, callback2){
 		request(options, callback)
 	}, 1000);
 	
-}
+}*/
+
+var MsTranslator = require('mstranslator');
+// Second parameter to constructor (true) indicates that
+// the token should be auto-generated.
+var client = new MsTranslator({
+    client_id: "kinasbot", client_secret: "ie523jAiAZQdISW0eEzXN0zxXTjnP5cbZZAdG9fPfn8="
+}, true);
+
+var params = {
+    text: 'test', 
+    from: 'en',
+    to: 'pt'
+};
 
 //=========================================================
 // Bot Setup
@@ -94,8 +106,6 @@ dialog.matches('TvProgramming', [
         session.dialogData.program = time;
         if (!channel) {
             var msg = "What is the channel?";
-            if (user_details.lang == "pt")
-                msg = "Qual é o canal?";
             builder.Prompts.text(session, msg);
             session.dialogData.channelQuery = session.message.text;
         } else {
@@ -114,11 +124,7 @@ dialog.matches('TvProgramming', [
                 request("http://nos-brpx.northeurope.cloudapp.azure.com/EPGRepositories/EPGCatalog.svc/Channel?$format=json", function (error, response, item) {
                     if (!error && response.statusCode == 200) {
                         var channelsf = JSON.parse(item).value;
-                        /*//console.log(channelsf);
-                         channelsf.forEach(function(channel) {
-                         channels.set(channel.Name.toLowerCase().replace(" ", ""), channel.ChannelId);
-                         })
-                         }*/
+
                         var min = {
                             value: 20,
                             channels: [{
@@ -154,26 +160,21 @@ dialog.matches('TvProgramming', [
                             min.channels.forEach(function (channel) {
                                 str += "\n" + channel.name;
                             });
-                            if (user_details.lang == "en") {
-                                session.send("Please be more specific in the channel, we found this channels:" + str);
-                            } else {
-                                session.send("Por favor sê mais especifico:" + str);
-                            }
+                            session.send("Please be more specific in the channel, we found this channels:" + str);
+                         
                             return;
                         }
                         if (min.channels.length == 1) {
                             channelResult = min.channels[0].id;
                         }
                         if (min.channels.length == 0) {
-                            if (user_details.lang == "en") {
-                                session.send("We couldn't find that channel!");
-                            } else {
-                                session.send("Não encontramos esse canal!");
-                            }
+                    
+                            session.send("We couldn't find that channel!");
+                       
                             return;
                         }
                     }
-                    console.log("CHANNEL TO SEARCH: " + channelResult);
+
                     request("http://nos-brpx.northeurope.cloudapp.azure.com/EPGRepositories/EPGCatalog.svc/Event?$format=json&$filter=ServiceId%20eq%20%27" + channelResult + "%27%20and%20UtcBeginDate%20ge%20datetime%27" + beginDate.toISOString() + "%27%20and%20UtcEndDate%20lt%20datetime%27" + beginDate.addHours(6).toISOString() + "%27&$expand=Program", function (error, response, item) {
                         if (!error && response.statusCode == 200) {
                             var programs = JSON.parse(item).value;
@@ -201,7 +202,7 @@ dialog.matches('TvProgramming', [
                 });
             }(results.response, finalTime));
         } else {
-            session.send("Ok");
+            session.send("Something, somewhere, went very wrong. Please try again.");
         }
     }
 ]);
@@ -209,11 +210,10 @@ dialog.matches('TvProgramming', [
 dialog.matches('Weather', [
     function(session, args, next) {
         var location = builder.EntityRecognizer.findEntity(args.entities, 'Place');
-
+        var when = builder.EntityRecognizer.resolveTime(args.entities);
+        session.dialogData.whenWeather = new Date(when);
         if (!location) {
             var msg = "What is the location?";
-            if(user_details.lang == "pt")
-              msg ="Qual é a localização?";
             builder.Prompts.text(session, msg);
         } else {
             next({
@@ -226,26 +226,32 @@ dialog.matches('Weather', [
 
             request('http://maps.google.com/maps/api/geocode/json?address=' + results.response, function(error, response, loc) {
                 if (!error && response.statusCode == 200) {
-                    request('https://api.darksky.net/forecast/95387f12434abcb72c983150ec9b7ab7/' + JSON.parse(loc).results[0].geometry.location.lat + "," +
-                        JSON.parse(loc).results[0].geometry.location.lng,
-                        function(error, response, item) {
-                            var msg = "Currently the weather is  " + JSON.parse(item).currently.summary.toLowerCase() + " and the probability of rain is: " + JSON.parse(item).currently.precipProbability + "%";
-                            if(user_details.lang=="pt")
-                                unbabel_submit(msg, 'en', 'pt', function(retorno){
-                                    unbabel_get_result(retorno, function(resultado){
-                                        session.send(resultado);
-                                    });
-                                });
-                            else 
+                    if(!session.dialogData.whenWeather){
+                        request('https://api.darksky.net/forecast/95387f12434abcb72c983150ec9b7ab7/' + JSON.parse(loc).results[0].geometry.location.lat + "," +
+                            JSON.parse(loc).results[0].geometry.location.lng,
+                            function(error, response, item) {
+                                var msg = "Right now the weather is  " + JSON.parse(item).currently.summary.toLowerCase() + " and the probability of rain is " +
+                                 JSON.parse(item).currently.precipProbability*100 + "%";
                                 session.send(msg);
-
-                        });
+                            });
+                    }
+                    else{
+                        var req = 'https://api.darksky.net/forecast/95387f12434abcb72c983150ec9b7ab7/' + JSON.parse(loc).results[0].geometry.location.lat + "," +
+                            JSON.parse(loc).results[0].geometry.location.lng + ","+  session.dialogData.whenWeather.getTime()/1000 + "?exclude=currently,flags";
+                        console.log(req);
+                        request(req,
+                            function(error, response, item) {
+                                var msg = "The weather gonna be " + JSON.parse(item).hourly.summary.toLowerCase().replace(".","") + " and the probability of rain is: " +
+                                JSON.parse(item).hourly.data.precipProbability + "%";
+                                session.send(msg);
+                            });
+                    }
                 } else {
                     console.log(error);
                 }
             });
         } else {
-            session.send("Ok");
+            session.send("Something, somewhere, went very wrong. Please try again.");
         }
     }
 ]);
@@ -255,8 +261,6 @@ dialog.matches('ProductPrice', [
         var product = builder.EntityRecognizer.findEntity(args.entities, 'Product');
         if (!product) {
             var msg = "What is the product name?";
-            if(user_details.lang == "pt")
-              msg ="Qual é o produto?";
             builder.Prompts.text(session, msg);
         } else {
             next({
@@ -266,89 +270,56 @@ dialog.matches('ProductPrice', [
     },
     function(session, results) {
         if (results.response) {
-            request("http://apicolpixelcamp.azure-api.net//api/search?query=" + results.response + "&productsToRetrieve=20", function(error, response, item) {
-                if (!error && response.statusCode == 200) {
-                    var products = JSON.parse(item).ProductsFound;
-                    if (products.length === 0) {
-                        session.send('No products found for that keyword!');
-                    }
-                    suggestions = 5;
-                    if (products.length < 5) {
-                        suggestions = products.length;
-                    }
-                    var used_indexes = [];
-                    response = "";
-                    for (i = 0; i < suggestions; i++) {
-                        do {
-                            a = Math.floor((Math.random() * products.length) + 1);
-                            //console.log(a);
-                            //console.log(used_indexes);
+            params.text = results.response;
+            client.translate(params, function(err, data) {
+                console.log(data);
+                    request("http://apicolpixelcamp.azure-api.net//api/search?query=" + data + "&productsToRetrieve=20", function(error, response, item) {
+                    if (!error && response.statusCode == 200) {
+                        var products = JSON.parse(item).ProductsFound;
+                        if (products.length === 0) {
+                            session.send('No products found for that keyword!');
                         }
-                        while (!used_indexes.indexOf(a));
-                        used_indexes.push(a);
-                        response += (i + 1 + '.º ' + products[a].ProductBrand + " - " + products[a].ProductWebDisplayName + " - " + products[a].ProductOriginalListPrice + '€\n\n');
+                        suggestions = 5;
+                        if (products.length < 5) {
+                            suggestions = products.length;
+                        }
+                        var used_indexes = [];
+                        response = "";
+                        for (i = 0; i < suggestions; i++) {
+                            do {
+                                a = Math.floor((Math.random() * products.length) + 1);
+                                //console.log(a);
+                                //console.log(used_indexes);
+                            }
+                            while (!used_indexes.indexOf(a));
+                            used_indexes.push(a);
+                            response += (i + 1 + '.º ' + products[a].ProductBrand + " - " + products[a].ProductWebDisplayName + " - " + products[a].ProductOriginalListPrice + '€\n\n');
+                        }
+                        session.send(response);
                     }
-                    session.send(response);
-                }
+                });
             });
+
         } else {
-            session.send("Ok");
+            session.send("Something, somewhere, went very wrong. Please try again.");
         }
     }
 ]);
 
 dialog.matches('Greeting', [
     function (session) {
-        builder.Prompts.text(session, "Hello! What is your favorite language?(answer: en) \n\n Olá! Qual é a tua lingua favorita? (responde: pt)");
-    },
-    function (session, results, next) {
-        if (results.response) {
-            user_details.lang = results.response;
-            session.dialogData.lang = results.response;
-            if(session.dialogData.lang == "pt"){
-                builder.Prompts.text(session, "Como te chamas?");
-            }
-            else {
-                builder.Prompts.text(session, "What is your name?");
-            }
-            
-        } else {
-            next();
-        }
+        builder.Prompts.text(session, "Hello! I'm the KinasBot, what is your name?");
     },
     function (session, results) {
         if (results.response) {
-           user_details.name = results.response;
            session.dialogData.name = results.response;
         }
-        if(user_details.lang == "pt"){
-            session.send("Como posso ser útil, " + user_details.name + " ?");
-        }
-        else{
-            session.send("How can I be useful, " + user_details.name + " ?");
-        }
-        
+        session.send("How can I be useful, " +  session.dialogData.name + " ?");  
     }
 ]);
 
 dialog.onDefault(
     [function (session) {
-        if(session.dialogData.lang == "pt"){
-            var textvar = session.message.text;
-            console.log("in:  "+textvar)
-            unbabel_submit(textvar, 'pt', 'en', function(retorno){
-                unbabel_get_result(retorno, function(resultado){
-                    console.log(resultado);
-
-                    session.messsage = resultado;
-                
-                    console.log("sess: " + session.messsage.text);
-                    session.replaceDialog('/');
-                    next();
-                });
-            })
-        } else {
             session.send("Sorry, couldn't understand that.");
-        }
     }]
 );
